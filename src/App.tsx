e18354a272ae4154fb196adc8e2f1407892a011e
Gus <gourisankar.shaji@gmail.com>
2025-05-28 14:01:12 +0530

    added a couple of basic pages and preparing to build the components -- testing git branching

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,47 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./App.css";
-import CreateEmployee from "./pages/create-employee/CreateEmployee";
 import Login from "./pages/login/Login";
-import Layout from "./components/layout/Layout";
-import EmployeeDashboard from "./pages/employee-dashboard/EmployeeDashboard";
+import Dashboard from "./pages/employees/dashboard/Dashboard";
+import CreateEmployee from "./pages/employees/createEmployee/CreateEmployee";
+// import CreateEmployee from "./pages/create-employee/CreateEmployee";
+// import Login from "./pages/login/Login";
+// import Layout from "./components/layout/Layout";
+// import EmployeeDashboard from "./pages/employee-dashboard/EmployeeDashboard";
 
-export const isLoggedIn = () => {
-  const token = localStorage.getItem("login");
-  return token == "true";
-};
+// export const isLoggedIn = () => {
+//   const token = localStorage.getItem("login");
+//   return token == "true";
+// };
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: isLoggedIn() ? <Navigate to="/employees" /> : <Login />,
+//   },
+//   {
+//     path: "/login",
+//     element: isLoggedIn() ? <Navigate to="/employees" /> : <Login />,
+//   },
+//   {
+//     path: "/employees",
+//     element: <Layout />,
+//     children: [
+//       { index: true, element: <EmployeeDashboard /> },
+//       { path: "create", element: <CreateEmployee /> },
+//     ],
+//   },
+// ]);
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: isLoggedIn() ? <Navigate to="/employees" /> : <Login />,
-  },
-  {
-    path: "/login",
-    element: isLoggedIn() ? <Navigate to="/employees" /> : <Login />,
+    element: <Login />,
   },
   {
     path: "/employees",
-    element: <Layout />,
     children: [
-      { index: true, element: <EmployeeDashboard /> },
+      { index: true, element: <Dashboard /> },
       { path: "create", element: <CreateEmployee /> },
     ],
   },
