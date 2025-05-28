e758614bad836a73de2774197d84fae4b4f53d9b
Gus <gourisankar.shaji@gmail.com>
2025-05-28 14:52:31 +0530

    added styling to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,18 @@
 import "./Navbar.css";
+import empListIcon from "../../assets/icon.svg";
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <div className="navbar-menu-items">
-        <div className="navbar-menu-item employee-list">Employee List</div>
+        <div className="navbar-menu-item employee-list">
+          <div className="employee-list-icon-container navbar-menu-item-icon">
+            <img src={empListIcon} />
+          </div>
+          <div className="navbar-menu-item-text employee-list-text">
+            Employee List
+          </div>
+        </div>
       </div>
       <div>Logout</div>
     </div>
