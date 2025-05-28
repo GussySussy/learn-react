cde3ff74dbb1e361bdabb0d32687d0d22cc67920
Gus <gourisankar.shaji@gmail.com>
2025-05-28 20:36:52 +0530

    finished styling login page #gotta add the clear button

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,8 @@
 import "./Login.css";
 import loginImage from "../../assets/kv-login.jpeg";
+import kvLogo from "../../assets/kv-logo.png";
+import Input from "../../components/input/Input";
+import Button from "../../components/button/Button";
 
 const Login = () => {
   return (
@@ -9,7 +12,20 @@ const Login = () => {
           <img src={loginImage} className="login-image" />
         </div>
       </div>
-      <div className="login-section"> Login Section</div>
+      <div className="login-section">
+        <div className="login-content">
+          <div className="logo-image-container">
+            <img src={kvLogo} className="kv-logo" />
+          </div>
+          <div className="login-form-fields">
+            <Input labelText="Username" placeholder="Enter Username" />
+            <Input labelText="Password" placeholder="Enter Password" />
+          </div>
+          <div className="login-button-container">
+            <Button buttonText="Login" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
